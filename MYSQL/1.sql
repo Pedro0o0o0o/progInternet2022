fb04ec4d956af30add45a5e18e3c6465957f8c4f
Pedro Melo <pedro.pio.martins@hotmail.com>
2022-11-05 12:47:15 -0300

    aula 24

---
+++ MYSQL/1.sql
@@ -0,0 +1,6 @@
+insert into pizza(nome,descricao,valor) values ('Portuguesa','Portuguesa',15);
+insert into pizza(nome,descricao,valor) values ('Provolone','Provolone',17);
+insert into pizza(nome,descricao,valor) values ('4 queijos','4 queijos',20);
+insert into pizza(nome,descricao,valor) values ('Calabreza','Calabreza',17);
+insert into pizza(nome,descricao,valor) values ('Escarola','Escarola',15);
+
